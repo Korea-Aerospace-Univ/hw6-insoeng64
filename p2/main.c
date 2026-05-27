4d4178df32f1d7f491ec9257c1a854cb545eabaf
insoeng64 <108975160+insoeng64@users.noreply.github.com>
2026-05-27 15:42:49 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -5,14 +5,12 @@ void main() {
     int N = 0;
     scanf("%d", &N);
 
-    for(int *p = str; p < str + N; p++)
+    for (int* p = str; p < str + N; p++)
         scanf("%d", p);
-    for (int *p1 = str1; p1 < str1+N; p1++)
+    for (int* p1 = str1; p1 < str1 + N; p1++)
         scanf("%d", p1);
-    int* p1 = str1 + N;
-
-    for (int *p = str; p < str + N; p++) {
-        p1--;
+    
+    for (int* p = str, *p1 = str1+N-1; p < str + N; p++, p1--) {
         printf(" %d", *p + *p1);
     }
 }
